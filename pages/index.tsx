3c73224a0068f1266a98c63d82efc43551b2984a
Kazooooo <cest93@gmail.com>
2018-03-17 02:24:29 +0900

    CSS in JSでindexページをスタイリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,48 @@ import Link from "next/link";
 import Layout from "../components/MyLayout";
 import fetch from "isomorphic-unfetch";
 
-interface IndexProps {
-  shows: any[];
+function getPosts() {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" },
+  ]
 }
 
-const Index: React.SFC<IndexProps> = (props) => (
+export default () => (
   <Layout>
-    <h1>Batman TV Shows</h1>
+    <h1>My Blog</h1>
     <ul>
-      {props.shows.map(({show}) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
+      {getPosts().map((post) => (
+        <li key={post.id}>
+          <Link as={`/p/${post.id}`} href={`/post?title~${post.title}`}>
+            <a>{post.title}</a>
           </Link>
         </li>
       ))}
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
-)
-
-Index.getInitialProps = async function() {
-  const res = await fetch("http://api.tvmaze.com/search/shows?q=batman");
-  const data = await res.json();
-
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data,
-  };
-}
-
-export default Index;
+);
