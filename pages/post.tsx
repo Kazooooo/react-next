c595f993d96aa7f3bba2d42ec4ea8d179954e4af
Kazooooo <cest93@gmail.com>
2018-03-17 02:52:30 +0900

    Postページをglobal styleを使ってスタイリング

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,27 +1,45 @@
+import * as Markdown from "react-markdown";
 import Layout from "../components/MyLayout";
-import fetch from "isomorphic-unfetch";
 import { UrlComponent, Context } from "next";
 
 interface PostProps extends UrlComponent {
-  show: any;
 }
 
 const Post: React.SFC<PostProps> = (props) => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
-    <img src={props.show.image.medium} />
+    <h1>{props.url.query.title}</h1>
+    <div className="markdown">
+      <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link)
+And we can have a title as well.
+
+### this is a title
+
+And here's the content.
+      `}/>
+      <style jsx global>{`
+        .markdown {
+          font-family: "Arial";
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </div>
   </Layout>
 );
-
-Post.getInitialProps = async function (context: Context) {
-  const { id } = context.query;
-  const res = await fetch(`http://api.tvmaze.com/shows/${id}`);
-  const show = await res.json();
-
-  console.log(`Fetched show: ${show.name}`);
-
-  return { show };
-}
 
 export default Post;
