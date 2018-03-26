43df6a9a9a04f0458117ddcae5182fb540bb5919
Kazooooo <cest93@gmail.com>
2018-03-27 01:53:53 +0900

    他のURL、クエリURLの追加

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ const withAwesomeTypescript = require("next-awesome-typescript");
 module.exports = withAwesomeTypescript({}, {
   exportPathMap: () => ({
       "/" : { page: "/" },
-  }),
+      "/about": { page: "/about" },
+      "/p/hello-nextjs": { page: "/post", query: { title: "Hello Next.js" } },
+      "/p/learn-nextjs": { page: "/post", query: { title: "Learn Next.js is awesome" } },
+      "/p/deploy-nextjs-nextjs": { page: "/post", query: { title: "Deploy apps with Zeit" } },
+      "/p/exporting-pages": { page: "/post", query: { title: "Learn to Export HTML Pages" } },
+    }),
 });
